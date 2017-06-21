792874a369372284f3a6dde0b6dc5bc03c9210a0
Cory LaFlamme <me@corylaflamme.com>
2017-06-21 13:59:38 -0400

    new build

--- docs/scripts/currency.js
+++ docs/scripts/currency.js
@@ -1,5 +1,8 @@
 (function() {
-  var Currency, c, denominationsList, fromPairs, generateDenominationMap;
+  var Currency, c, denominationsList, fromPairs, generateDenominationMap,
+    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
 
   c = React.createElement;
 
@@ -45,18 +48,28 @@
     }));
   };
 
-  Currency = React.createClass({
-    getInitialState: function() {
-      return {
+  Currency = (function(superClass) {
+    extend(Currency, superClass);
+
+    function Currency(props) {
+      this.renderCurrencyColumn = bind(this.renderCurrencyColumn, this);
+      Currency.__super__.constructor.call(this, props);
+      this.state = {
         inputs: generateDenominationMap(),
         bestFit: generateDenominationMap()
       };
-    },
-    onChange: function(denominationKey, e) {
-      this.state.inputs[denominationKey] = parseInt(e.target.value || 0);
-      return this.setState(this.state);
-    },
-    renderSmallest: function() {
+    }
+
+    Currency.prototype.onChange = function(denominationKey) {
+      return (function(_this) {
+        return function(e) {
+          _this.state.inputs[denominationKey] = parseInt(e.target.value || 0);
+          return _this.setState(_this.state);
+        };
+      })(this);
+    };
+
+    Currency.prototype.renderSmallest = function() {
       var denominations, sortedDenominations, total;
       total = denominationsList.reduce((function(_this) {
         return function(total, denomination) {
@@ -79,34 +92,40 @@
           className: "currency-results__result currency-results__result--" + denomination.key
         }, "" + denominations[denomination.key] + denomination.key + " ");
       });
-    },
-    renderConversionDropdown: function() {
+    };
+
+    Currency.prototype.renderCurrencyColumn = function(denomination, i) {
+      return c('div', {
+        key: i,
+        className: "currency-column currency-column--" + denomination.key
+      }, c('div', {
+        className: 'currency-label'
+      }, denomination.label), c('input', {
+        type: 'number',
+        className: 'currency-input',
+        onChange: this.onChange(denomination.key)
+      }), c('div', {
+        className: 'currency-key'
+      }, denomination.key));
+    };
+
+    Currency.prototype.renderConversionDropdown = function() {
       return c('select', {
         className: 'currency-output-dropdown'
       }, c('option', null, 'Most efficient (smallest number of coins)'));
-    },
-    render: function() {
+    };
+
+    Currency.prototype.render = function() {
       return c('form', null, c('div', {
         className: 'currency'
-      }, denominationsList.map((function(_this) {
-        return function(denomination) {
-          return c('div', {
-            className: "currency-column currency-column--" + denomination.key
-          }, c('div', {
-            className: 'currency-label'
-          }, denomination.label), c('input', {
-            type: 'text',
-            className: 'currency-input',
-            onChange: _this.onChange.bind(_this, denomination.key)
-          }), c('div', {
-            className: 'currency-key'
-          }, denomination.key));
-        };
-      })(this))), c('div', null, this.renderConversionDropdown(), c('div', {
+      }, denominationsList.map(this.renderCurrencyColumn)), c('div', null, this.renderConversionDropdown(), c('div', {
         className: 'currency-results'
       }, this.renderSmallest())));
-    }
-  });
+    };
+
+    return Currency;
+
+  })(React.Component);
 
   ReactDOM.render(c(Currency), document.querySelector('#currency-container'));
 
