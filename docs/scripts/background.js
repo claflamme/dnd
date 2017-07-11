eb415ed7ebe41d90cb3718a3fe9d08233cd5a365
Cory LaFlamme <me@corylaflamme.com>
2017-07-11 18:04:04 -0400

    Added new build

--- docs/scripts/background.js
+++ docs/scripts/background.js
@@ -6,7 +6,7 @@ var BackgroundGenerator, backgroundsList, el, getRandomBackground, getRandomFrom
 
 el = React.createElement;
 
-backgroundsList = [require('./backgrounds/acolyte'), require('./backgrounds/charlatan'), require('./backgrounds/criminal'), require('./backgrounds/entertainer'), require('./backgrounds/folk-hero')];
+backgroundsList = [require('./backgrounds/acolyte'), require('./backgrounds/charlatan'), require('./backgrounds/criminal'), require('./backgrounds/entertainer'), require('./backgrounds/folk-hero'), require('./backgrounds/guild-artisan')];
 
 getRandomFromList = function(list) {
   return list[Math.floor(Math.random() * list.length)];
@@ -60,13 +60,13 @@ BackgroundGenerator = (function(superClass) {
     }, title), el('p', null, text));
   };
 
-  BackgroundGenerator.prototype.renderIdeal = function(summary, text) {
-    return el('span', null, el('strong', null, summary + ": "), text);
+  BackgroundGenerator.prototype.renderIdeal = function(summary, alignment, text) {
+    return el('span', null, el('strong', null, summary + ": "), text, " (" + alignment + ")");
   };
 
   BackgroundGenerator.prototype.renderDetails = function() {
     var ideal;
-    ideal = this.renderIdeal(this.state.ideal.summary, this.state.ideal.text);
+    ideal = this.renderIdeal(this.state.ideal.summary, this.state.ideal.alignment, this.state.ideal.text);
     return el('div', {
       className: 'background-details'
     }, el('div', {
@@ -118,7 +118,7 @@ BackgroundGenerator = (function(superClass) {
 ReactDOM.render(el(BackgroundGenerator), document.querySelector('#background-container'));
 
 
-},{"./backgrounds/acolyte":2,"./backgrounds/charlatan":3,"./backgrounds/criminal":4,"./backgrounds/entertainer":5,"./backgrounds/folk-hero":6}],2:[function(require,module,exports){
+},{"./backgrounds/acolyte":2,"./backgrounds/charlatan":3,"./backgrounds/criminal":4,"./backgrounds/entertainer":5,"./backgrounds/folk-hero":6,"./backgrounds/guild-artisan":7}],2:[function(require,module,exports){
 module.exports = {
   name: "Acolyte",
   source: "Player's Handbook",
@@ -666,6 +666,116 @@ module.exports = {
     }, {
       quip: "is bored of their mundane existence",
       text: "I get bored easily. When am I going to get on with my destiny?"
+    }
+  ]
+};
+
+
+},{}],7:[function(require,module,exports){
+module.exports = {
+  name: "Guild Artisan",
+  source: "Player's Handbook",
+  quip: "craftsman",
+  flaws: [
+    {
+      quip: "scheming",
+      text: "I’ll do anything to get my hands on something rare or priceless."
+    }, {
+      quip: "apprehensive",
+      text: "I’m quick to assume that someone is trying to cheat me."
+    }, {
+      quip: "thieving",
+      text: "No one must ever learn that I once stole money from guild coffers."
+    }, {
+      quip: "covetous",
+      text: "I’m never satisfied with what I have—I always want more."
+    }, {
+      quip: "power hungry",
+      text: "I would kill to acquire a noble title."
+    }, {
+      quip: "insecure",
+      text: "I’m horribly jealous of anyone who can outshine my handiwork. Everywhere I go, I’m surrounded by rivals."
+    }
+  ],
+  bonds: [
+    {
+      quip: "that can't leave home behind",
+      text: "The workshop where I learned my trade is the most important place in the world to me."
+    }, {
+      quip: "that thinks they're better than others",
+      text: "I created a great work for someone, and then found them unworthy to receive it. I’m still looking for someone worthy."
+    }, {
+      quip: "that loves their guild a little too much",
+      text: "I owe my guild a great debt for forging me into the person I am today."
+    }, {
+      quip: "who uses money to buy friends",
+      text: "I pursue wealth to secure someone’s love."
+    }, {
+      quip: "with something to prove",
+      text: "One day I will return to my guild and prove that I am the greatest artisan of them all."
+    }, {
+      quip: "on a quest for vengeance",
+      text: "I will get revenge on the evil forces that destroyed my place of business and ruined my livelihood."
+    }
+  ],
+  ideals: [
+    {
+      quip: "believes in order and security",
+      summary: "Community",
+      text: "It is the duty of all civilized people to strengthen the bonds of community and the security of civilization.",
+      alignment: "Lawful"
+    }, {
+      quip: "thinks they can solve the world's problems",
+      summary: "Generosity",
+      text: "My talents were given to me so that I could use them to benefit the world.",
+      alignment: "Good"
+    }, {
+      quip: "has revolutionary tendencies",
+      summary: "Freedom",
+      text: "Everyone should be free to pursue his or her own livelihood.",
+      alignment: "Chaotic"
+    }, {
+      quip: "is only in it for the money",
+      summary: "Greed",
+      text: "I’m only in it for the money.",
+      alignment: "Evil"
+    }, {
+      quip: "doesn't buy in to the establishment",
+      summary: "People",
+      text: "I’m committed to the people I care about, not to ideals. ",
+      alignment: "Neutral"
+    }, {
+      quip: "a total workaholic",
+      summary: "Aspiration",
+      text: "I work hard to be the best there is at my craft.",
+      alignment: "Any"
+    }
+  ],
+  personalities: [
+    {
+      quip: "fancies themselves a perfectionist",
+      text: "I believe that anything worth doing is worth doing right. I can’t help it— I’m a perfectionist."
+    }, {
+      quip: "is an insufferable snob",
+      text: "I’m a snob who looks down on those who can’t appreciate fine art."
+    }, {
+      quip: "always gets up in everyone's business",
+      text: "I always want to know how things work and what makes people tick."
+    }, {
+      quip: "spouts obvious truisms to feel wise",
+      text: "I’m full of witty aphorisms and have a proverb for every occasion."
+    }, {
+      quip: "hates lazy people",
+      text: "I’m rude to people who lack my commitment to hard work and fair play."
+    }, {
+      quip: "only ever talks about work",
+      text: "I like to talk at length about my profession."
+    }, {
+      quip: "is incredibly cheap",
+      text: "I don’t part with my money easily and will haggle tirelessly to get the best deal possible."
+    }, {
+      quip: "has a giant ego",
+      text: "I’m well known for my work, and I want to make sure everyone appreciates it. I'm always taken aback when people haven’t heard of me."
     }
   ]
 };
